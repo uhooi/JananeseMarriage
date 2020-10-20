b7c95a3a862ba8c278d01c26c55fbadc2e8e6277
saki iwamoto <saki.iwamoto.work@gmail.com>
2020-10-21 00:23:45 +0900

    Enumクラス追加
    * 性別Enum追加

---
+++ JananeseMarriage/JananeseMarriage/Const/Enum.swift
@@ -0,0 +1,16 @@
+//
+//  Enum.swift
+//  JananeseMarriage
+//
+//  Created by saki iwamoto on 2020/10/21.
+//
+
+import Foundation
+
+/// 性別
+enum Gender {
+    case Notknown
+    case Male
+    case Female
+    case NotApplicable
+}
