d29aeb623d2da620ab5810d76b4c024dd913d19d
saki iwamoto <saki.iwamoto.work@gmail.com>
2020-10-21 00:11:35 +0900

    プロジェクト作成

---
+++ JananeseMarriage/JananeseMarriage/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JananeseMarriage
+//
+//  Created by saki iwamoto on 2020/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
